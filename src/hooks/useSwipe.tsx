60d3378cc0e1b4790e970e425098ce37a2085f5a
rain <1063044718@qq.com>
2023-03-03 09:59:05 +0800

    添加swipe

--- src/hooks/useSwipe.tsx
+++ src/hooks/useSwipe.tsx
@@ -1,51 +1,58 @@
-import {Ref, computed, onMounted, ref} from 'vue'
+import { Ref, computed, onMounted, onUnmounted, ref } from 'vue'
 type Point = {
     x: number
     y: number
 }
 
 export const useSwipe = (element: Ref<HTMLElement | null>) => {
-    const start = ref<Point| null>(null)
-    const end = ref<Point| null>(null)
+    const start = ref<Point | null>(null)
+    const end = ref<Point | null>(null)
     const swiping = ref(false)
     const distance = computed(() => {
-        if(!start.value || !end.value) return null
+        if (!start.value || !end.value) return null
         return {
             x: end.value.x - start.value.x,
             y: end.value.y - start.value.y,
         }
     })
-    const direction = computed(()=> {
+    const direction = computed(() => {
         if (!distance.value) return ''
-        const {x, y} = distance.value
-        if(Math.abs(x) > Math.abs(y)) {
+        const { x, y } = distance.value
+        if (Math.abs(x) > Math.abs(y)) {
             return x > 0 ? 'right' : 'left'
         } else {
             return y > 0 ? 'down' : 'up'
         }
-    }) 
-
-    onMounted(() => {
-        element.value?.addEventListener('touchstart', e => {
-            swiping.value = true
-            start.value = {
-                x: e.touches[0].clientX,
-                y: e.touches[0].clientY
-            }
-            end.value = null
-        })
-        element.value?.addEventListener('touchmove', e => {
-            end.value = {
-                x: e.touches[0].clientX,
-                y: e.touches[0].clientY
-            }
-        })
-        element.value?.addEventListener('touchend', e => {
-            swiping.value = false
-            start.value = null
-            end.value = null
-        })
     })
 
-    return {distance, direction, swiping}
+    const onStart = (e: TouchEvent) => {
+        swiping.value = true
+        end.value = start.value = { x: e.touches[0].screenX, y: e.touches[0].screenY }
+    }
+    const onMove = (e: TouchEvent) => {
+        if (!start.value) { return }
+        end.value = { x: e.touches[0].screenX, y: e.touches[0].screenY, }
+    }
+    const onEnd = (e: TouchEvent) => {
+        swiping.value = false
+        start.value = null
+        end.value = null
+    }
+
+    onMounted(() => {
+        if (!element.value) return
+        element.value.addEventListener('touchstart', onStart)
+        element.value.addEventListener('touchmove', onMove)
+        element.value.addEventListener('touchend', onEnd)
+    })
+
+    onUnmounted(() => {
+        if (!element.value) return
+        element.value.removeEventListener('touchstart', onStart)
+        element.value.removeEventListener('touchmove', onMove)
+        element.value.removeEventListener('touchend', onEnd)
+    })
+
+
+    return { distance, direction, swiping }
 }
