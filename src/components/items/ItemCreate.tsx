2c10bad1dad6599bec9a854448c51cb15d6ebaa4
rain <1063044718@qq.com>
2023-03-11 16:29:50 +0800

    完成tag的样式

--- src/components/items/ItemCreate.tsx
+++ src/components/items/ItemCreate.tsx
@@ -1,33 +1,69 @@
-import { defineComponent, PropType, ref } from 'vue';
-import s from './ItemCreate.module.scss';
-import { MainLayout } from '../../layouts/MainLayout';
-import { Icon } from '../../shared/Icon';
-import { Tabs, Tab } from '../../shared/Tabs';
-import { InputPad } from './InputPad';
+import { defineComponent, PropType, ref } from "vue";
+import s from "./ItemCreate.module.scss";
+import { MainLayout } from "../../layouts/MainLayout";
+import { Icon } from "../../shared/Icon";
+import { Tabs, Tab } from "../../shared/Tabs";
+import { InputPad } from "./InputPad";
 export const ItemCreate = defineComponent({
-    props: {
-        name: {
-            type: String as PropType<string>
-        }
+  props: {
+    name: {
+      type: String as PropType<string>,
     },
-    setup: (props, context) => {
-        const refKind = ref('支出')
-        return () => (
-            <MainLayout>{
-                {
-                    title: () => '记一笔',
-                    icon: () => <Icon name="left" />,
-                    default: () => <>
-                            <Tabs v-model:selected={refKind.value}>
-                                <Tab name="支出">icon列表1</Tab>
-                                <Tab name="收入">icon列表2</Tab>
-                            </Tabs>
-                            <div class={s.inputPad_wrapper}>
-                            <InputPad />
-                            </div>
-                        </>
-                }
-            }</MainLayout>
-        )
-    }
-})
+  },
+  setup: (props, context) => {
+    const refKind = ref("支出");
+    const refExpensesTags = ref([
+      { id: 1, name: "餐费", sign: "￥", category: "expenses" },
+    ]);
+    const refIncomeTags = ref([
+      { id: 1, name: "工资", sign: "￥", category: "incoome" },
+    ]);
+    return () => (
+      <MainLayout>
+        {{
+          title: () => "记一笔",
+          icon: () => <Icon name="left" />,
+          default: () => (
+            <>
+              <div class={s.wrapper}>
+                <Tabs v-model:selected={refKind.value} class={s.tabs}>
+                  <Tab name="支出" class={s.tags_wrapper}>
+                    <div class={s.tag}>
+                      <div class={s.sign}>
+                        <Icon name="add" class={s.createTag} />
+                      </div>
+                      <div class={s.name}>新增</div>
+                    </div>
+                    {refExpensesTags.value.map((tag) => (
+                      <div class={[s.tag, s.selected]}>
+                        <div class={s.sign}>{tag.sign}</div>
+                        <div class={s.name}>{tag.name}</div>
+                      </div>
+                    ))}
+                  </Tab>
+                  <Tab name="收入" class={s.tags_wrapper}>
+                    <div class={s.tag}>
+                      <div class={s.sign}>
+                        <Icon name="add" class={s.createTag} />
+                      </div>
+                      <div class={s.name}>新增</div>
+                    </div>
+                    {refIncomeTags.value.map((tag) => (
+                      <div class={[s.tag, s.selected]}>
+                        <div class={s.sign}>{tag.sign}</div>
+                        <div class={s.name}>{tag.name}</div>
+                      </div>
+                    ))}
+                  </Tab>
+                </Tabs>
+                <div class={s.inputPad_wrapper}>
+                  <InputPad />
+                </div>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
