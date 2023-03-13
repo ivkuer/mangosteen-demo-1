0ae975615024f2c1bf55a76827824b47bdc6daf0
rain <1063044718@qq.com>
2023-03-13 16:33:28 +0800

    bug

---
+++ src/components/statistics/Charts.tsx
@@ -0,0 +1,18 @@
+import { defineComponent, PropType } from 'vue';
+export const Charts = defineComponent({
+  props: {
+    startDate: {
+      type: String as PropType<string>,
+      required: true
+    },
+    endDate: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  setup: (props, context) => {
+    return () => (
+      <div>chaty</div>
+    )
+  }
+})
