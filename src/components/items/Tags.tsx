78bb4b520c2928608aed9a6460bf1c4dde530eb4
rain <1063044718@qq.com>
2023-03-19 13:18:08 +0800

    触发长按逻辑

--- src/components/items/Tags.tsx
+++ src/components/items/Tags.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './Tags.module.scss';
 import { Icon } from '../../shared/Icon';
 import { Button } from '../../shared/Button';
@@ -21,8 +21,29 @@ export const Tags = defineComponent({
     const onSelect = (tag: Tag) => { 
       context.emit('update:selected', tag.id)
     }
+    const timer = ref<number>()
+    const currentTag = ref<HTMLDivElement>()
+    const onLongPress = () => {
+      console.log('长按');
+      
+    }
+    const onTouchStart = (e: TouchEvent) => {
+      currentTag.value = e.currentTarget as HTMLDivElement
+      timer.value = setTimeout(() => {
+        onLongPress()
+      }, 1000)
+    }
+    const onTouchMove = (e: TouchEvent) => {
+      const pointedElement = document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY)
+      if (currentTag.value !== pointedElement && currentTag.value?.contains(pointedElement) === false) {
+        clearTimeout(timer.value)
+      }
+    }
+    const onTouchEnd = (e: TouchEvent) => {
+      clearTimeout(timer.value)
+    }
     return () => (<>
-      <div class={s.tags_wrapper}>
+      <div class={s.tags_wrapper} onTouchmove={onTouchMove}>
         <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
           <div class={s.sign}>
             <Icon name="add" class={s.createTag} />
@@ -32,6 +53,8 @@ export const Tags = defineComponent({
         {tags.value.map((tag) => (
           <div class={[s.tag, props.selected === tag.id ? s.selected : '']}
           onClick={() => onSelect(tag)}
+          onTouchstart={onTouchStart}
+          onTouchend={onTouchEnd}
           >
             <div class={s.sign}>{tag.sign}</div>
             <div class={s.name}>{tag.name}</div>
