2ce6f05a1c3e5db0d07237a3edfe9c8dd6146410
rain <1063044718@qq.com>
2023-03-23 12:28:00 +0800

    api接口地址更改

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://121.196.236.94:8080/'
+        target: 'https://mangosteen2.hunger-valley.com/'
       }
     }
   }
