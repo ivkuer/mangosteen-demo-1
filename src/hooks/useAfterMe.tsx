d7888617a608c0791b4ec1575ca6e218c3d82cda
rain <1063044718@qq.com>
2023-03-23 12:46:25 +0800

    解决vue关于onMounted中不允许有未处理的错误的警告

--- src/hooks/useAfterMe.tsx
+++ src/hooks/useAfterMe.tsx
@@ -3,8 +3,6 @@ import { useMeStore } from "../stores/useMeStore"
 
 export const useAfterMe = (fn: () => void) => {
   const meStore = useMeStore()
-  onMounted(async () => {
-    await meStore.mePromise
-    fn()
-  })
-}
+  onMounted(() => {
+    meStore.mePromise!.then(fn, () => undefined)
+  })}
